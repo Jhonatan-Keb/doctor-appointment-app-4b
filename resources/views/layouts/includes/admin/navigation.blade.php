3d9408a69f8a722335348aa2bd3ea3a2e4010606
Jhonatan-Keb <jhonykeb123@gmail.com>
2025-09-22 01:05:26 -0600

    feat(admin): add admin layout with Flowbite sidebar, navigation includes, and dashboard slot Version 3

--- resources/views/layouts/includes/admin/navigation.blade.php
+++ resources/views/layouts/includes/admin/navigation.blade.php
@@ -9,7 +9,7 @@
                         </svg>
                     </button>
                     <a href="/" class="flex ms-2 md:me-24">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Fedora_logo.svg/267px-Fedora_logo.svg.png" class="h-8 me-3" alt="FlowBite Logo" />
                     <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
                     </a>
                 </div>
