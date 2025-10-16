72cdeab585f12ea806ca25f30dceff942c005d91
Jhonatan-Keb <jhonykeb123@gmail.com>
2025-10-15 18:01:15 -0600

    feat(layout): add breadcrum component and integrate into admin layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,16 @@
-<x-admin-layout>
+<x-admin-layout :breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'href' => route('admin.dashboard'),
+    ],
+    [
+        'name' => 'Sid',
+        'href' => route('admin.dashboard'),
+    ],
+
+    [
+        'name' => 'Versrappen'
+    ],
+]">
     Hola desde admin
 </x-admin-layout>
